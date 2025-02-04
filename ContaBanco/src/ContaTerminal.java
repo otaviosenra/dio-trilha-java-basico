f6a0e9b1f8b5e74f439fffefb2c831c8c4e16b46
Otavio <araujofletes@gmail.com>
2025-02-04 14:22:06 -0300

    pronto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,36 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); 
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String cliente = scanner.nextLine();
+
+        BigDecimal saldo = null;
+        boolean saldoValido = false;
+        while (!saldoValido) {
+            System.out.println("Por favor, digite o saldo da Conta:");
+            try {
+                saldo = scanner.nextBigDecimal();
+                saldoValido = true;
+            } catch (Exception e) {
+                System.out.println("Valor inválido! Digite um número válido (use virgula como separador decimal).");
+                scanner.nextLine();
+            }
+        }
+
+        scanner.close();
+
+        System.out.println("\nOlá " + cliente + ", obrigado por criar uma conta em nosso banco, sua agência é " 
+            + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
